b42a49a5a91ea1d371c1d390ef176dc7f909f46a
Cruz06 <Olarut72@gmail.com>
2024-04-14 23:27:24 +0300

    Stepik restore

--- Selen_stepik/Part_3/test_3_6_7_parser_browser.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def test_guest_should_see_login_link(browser2):
-    browser2.get(link)
-    browser2.find_element(By.CSS_SELECTOR, "#login_link")
